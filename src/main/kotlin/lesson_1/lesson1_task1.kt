6e3b80b9bcce4a9e441914ea180bf6b0eb916188
tutyperefruty <tutyperefruty@gmail.com>
2025-10-15 14:14:48 +0900

    KS-1-1 create 2 variables

---
+++ src/main/kotlin/lesson_1/lesson1_task1.kt
@@ -0,0 +1,12 @@
+package org.example.lesson_1
+
+fun main() {
+    var totalOrders: Int = 50
+    val thanksMessage: String = "You are the Best buyer!"
+
+//    println(totalOrders)
+    println(thanksMessage)
+
+    totalOrders = 75
+    println(totalOrders)
+}
