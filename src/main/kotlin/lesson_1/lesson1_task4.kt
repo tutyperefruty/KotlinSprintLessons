91f07443da22659f50945f8f84cccf9a47018faa
tutyperefruty <tutyperefruty@gmail.com>
2025-10-16 12:40:48 +0900

    KS-1-4 Type

---
+++ src/main/kotlin/lesson_1/lesson1_task4.kt
@@ -0,0 +1,17 @@
+package org.example.lesson_1
+
+fun main() {
+    val length: ULong = 40868600000uL
+    val age: UByte = 27u
+    val partOfDay: Float = 0.075f
+    val seconds: UInt = 6480u
+    val partOfYear: Double = 0.000205479453
+    val apogee: Int = 327000
+
+    println(length)
+    println(age)
+    println(partOfDay)
+    println(seconds)
+    println(partOfYear)
+    println(apogee)
+}
