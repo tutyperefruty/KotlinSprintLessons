03f69d2fd98bf8c7a93708748814dab97603ca77
tutyperefruty <tutyperefruty@gmail.com>
2025-10-17 07:31:44 +0900

    KS-1-2 refactor

--- src/main/kotlin/lesson_1/lesson1_task2.kt
+++ src/main/kotlin/lesson_1/lesson1_task2.kt
@@ -7,19 +7,12 @@ fun main() {
     println(thanksMessage)
     println(totalOrders)
 
-//    Устанавливаем количество работников
     var numbersOfEmployees = 2000
 
-//    Количество работников: вывод на экран
     println("Numbers of Employees: $numbersOfEmployees")
-
 
 //    Увольняется 1 работник
     val fired = 1
     var renewNumbersOfEmployees = numbersOfEmployees - fired
     println("Numbers of Employees now: $renewNumbersOfEmployees")
-//or
-    println("OR")
-    numbersOfEmployees -=1
-    println("Numbers of Employees now: $numbersOfEmployees")
 }
