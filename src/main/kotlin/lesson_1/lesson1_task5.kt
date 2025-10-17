1ec0ae77f88cb82e5b2c87867d80aa6bb5782317
tutyperefruty <tutyperefruty@gmail.com>
2025-10-17 10:37:49 +0900

    KS-1-5 refactor

--- src/main/kotlin/lesson_1/lesson1_task5.kt
+++ src/main/kotlin/lesson_1/lesson1_task5.kt
@@ -1,21 +1,14 @@
 package org.example.lesson_1
 
 fun main() {
-//    01:48:00
     val flightTotalSecs = 6480
     val mountForMinOrSec = 60
 
     val flightTotalMinutes = flightTotalSecs/mountForMinOrSec
-//    println(flightTotalMinutes)
-
     val ostatokMinutes = flightTotalMinutes % mountForMinOrSec
-//    println(ostatokMinutes)
-
     val flightTotalHours = (flightTotalMinutes-ostatokMinutes)/mountForMinOrSec
-//    println(flightTotalHours)
-
     val ostatokSeconds = flightTotalSecs % mountForMinOrSec
-//    println(ostatokSeconds)
 
-    println("\nGagarin\'s time in cosmos is 0$flightTotalHours:$ostatokMinutes:0$ostatokSeconds")
+    val time = String.format("%02d:%02d:%02d", flightTotalHours, ostatokMinutes, ostatokSeconds)
+    println("\nGagarin\'s time in cosmos is $time")
 }
