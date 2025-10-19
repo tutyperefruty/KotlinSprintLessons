8ac93d1eadb0b59fb592f8873cdfe650e4ea6362
tutyperefruty <tutyperefruty@gmail.com>
2025-10-19 17:34:33 +0900

    KS-3-1 Greeting

---
+++ src/main/kotlin/lesson_3/lesson3_task1.kt
@@ -0,0 +1,10 @@
+package org.example.lesson_3
+
+fun main() {
+    val name = "Pororo"
+    var greeting = "Good day"
+    println("$greeting, $name!")
+
+    greeting = "Good evening"
+    println("$greeting, $name!")
+}
