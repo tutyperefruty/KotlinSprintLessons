b78134daac6a1e9afcaa29a319c847bf02d3bf62
tutyperefruty <tutyperefruty@gmail.com>
2025-10-20 08:31:31 +0900

    KS-2-1 average

---
+++ src/main/kotlin/lesson_2/lesson2_task1.kt
@@ -0,0 +1,15 @@
+package org.example.lesson_2
+
+fun main() {
+
+    val a: Float = 3f
+    val b: Float = 4f
+    val c: Float = 3f
+    val d: Float = 5f
+
+    val sum = a + b + c + d
+    val average = sum/4
+
+    println("$average")
+
+}
