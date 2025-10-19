c3977c3f75fa47c83a023e3bb6165d63f40e32cc
tutyperefruty <tutyperefruty@gmail.com>
2025-10-19 17:07:04 +0900

    KS-1-1 refactor-2

--- src/main/kotlin/lesson_1/lesson1_task1.kt
+++ src/main/kotlin/lesson_1/lesson1_task1.kt
@@ -1,8 +1,6 @@
 package org.example.lesson_1
 
 fun main() {
-    var totalOrders: Int = 50
+    var totalOrders: Int = 75
     val thanksMessage: String = "You are the Best buyer!"
-
-    totalOrders = 75
 }
