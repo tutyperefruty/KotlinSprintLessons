d69525ef68fc65b039537df194340e83b68f57ef
tutyperefruty <tutyperefruty@gmail.com>
2025-10-21 00:14:50 +0900

    KS-3-3 multiplication

---
+++ src/main/kotlin/lesson_3/lesson3_task3.kt
@@ -0,0 +1,9 @@
+package org.example.lesson_3
+
+fun main() {
+
+    val a = 7
+    var b = 0
+
+    println("$a * ${++b} = ${a * b}\n$a * ${++b} = ${a * b}\n$a * ${++b} = ${a * b}\n$a * ${++b} = ${a * b}\n$a * ${++b} = ${a * b}\n$a * ${++b} = ${a * b}\n$a * ${++b} = ${a * b}\n$a * ${++b} = ${a * b}\n$a * ${++b} = ${a * b}\n$a * ${++b} = ${a * b}")
+}
