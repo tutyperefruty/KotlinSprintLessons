2ca5b6b9e8d0ad68d580c22215b5ff4236c957c2
tutyperefruty <tutyperefruty@gmail.com>
2025-10-21 16:34:53 +0900

    KS-4-3 condition for growing

---
+++ src/main/kotlin/lesson_4/lesson4_task3.kt
@@ -0,0 +1,20 @@
+package org.example.lesson_4
+
+fun main() {
+
+    val ask = "Are the conditions now favorable for growing legumes?"
+    val nowWeather: String = "sun"
+    val nowIsTentOpened: Boolean = true
+    val nowHumidity: Int = 20
+    val nowWinter: Boolean = true
+
+    val result = (nowWeather == WEATHER) && (nowIsTentOpened == IS_TENT_OPENED) && (nowHumidity == HUMIDITY) && (nowWinter == IS_FOR_WINTER_SEASON)
+
+    println("$ask\n$result")
+
+}
+
+const val WEATHER: String = "sun"
+const val IS_TENT_OPENED: Boolean = true
+const val HUMIDITY: Int = 20
+const val IS_FOR_WINTER_SEASON: Boolean = false
