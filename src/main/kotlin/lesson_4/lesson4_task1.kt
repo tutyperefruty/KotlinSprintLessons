bac55d4c5e930865552eb21ca7b9a6b721dec3af
tutyperefruty <tutyperefruty@gmail.com>
2025-10-21 14:41:25 +0900

    KS-4-1 table availability

---
+++ src/main/kotlin/lesson_4/lesson4_task1.kt
@@ -0,0 +1,15 @@
+package org.example.lesson_4
+
+fun main() {
+
+    val todayTables = 13
+    val tomorrowTables = 9
+
+    val availableToday = TABLES != todayTables
+    val availableTomorrow = TABLES != tomorrowTables
+
+    println("[Table availability on today: $availableToday]\n[Table availability on tomorrow: $availableTomorrow]")
+
+}
+
+const val TABLES = 13
