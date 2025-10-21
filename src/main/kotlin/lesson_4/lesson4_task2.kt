0cf999032b96c8b39463b3e93bd1c334a83a1b84
tutyperefruty <tutyperefruty@gmail.com>
2025-10-21 15:57:22 +0900

    KS-4-2 customs category 'Average'

---
+++ src/main/kotlin/lesson_4/lesson4_task2.kt
@@ -0,0 +1,14 @@
+package org.example.lesson_4
+
+fun main() {
+
+    val weightA = 20
+    val litersA = 80
+    val weightB = 50
+    val litersB = 100
+
+    println("A: The cargo with a weight of $weightA kg and a volume of $litersA liters corresponds to the category \'Average\': ${(weightA > 35 && weightA <= 100) and (litersA < 100)}")
+    println("B: The cargo with a weight of $weightB kg and a volume of $litersB liters corresponds to the category \'Average\': ${(weightB > 35 && weightB <= 100) and (litersB < 100)}")
+
+
+}
