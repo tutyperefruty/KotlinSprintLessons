8954e8251916e23e652bf0a225809aadf377227c
tutyperefruty <tutyperefruty@gmail.com>
2025-10-22 10:07:13 +0900

    KS-2-1 refactor

--- src/main/kotlin/lesson_2/lesson2_task1.kt
+++ src/main/kotlin/lesson_2/lesson2_task1.kt
@@ -2,12 +2,12 @@ package org.example.lesson_2
 
 fun main() {
 
-    val a: Float = 3f
-    val b: Float = 4f
-    val c: Float = 3f
-    val d: Float = 5f
+    val num1: Float = 3f
+    val num2: Float = 4f
+    val num3: Float = 3f
+    val num4: Float = 5f
 
-    val sum = a + b + c + d
+    val sum = num1 + num2 + num3 + num4
     val average = sum/4
 
     println("$average")
