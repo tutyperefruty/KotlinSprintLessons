2c3864a620ba7b84ffd8501089b4ce9c0270f4e5
tutyperefruty <tutyperefruty@gmail.com>
2025-10-31 11:42:40 +0900

    KS-5-2 age compare

---
+++ src/main/kotlin/lesson_5/lesson5_task2.kt
@@ -0,0 +1,19 @@
+package org.example.lesson_5
+
+import java.time.Year
+
+const val AGE_OF_MAGORITY = 18
+
+fun main() {
+    println("Input your birth year: ")
+    val inputBirthYear = readLine()!!.toInt()
+
+    val nowYear = Year.now().value
+
+    val output = if (AGE_OF_MAGORITY >= nowYear - inputBirthYear) {
+        "Показать экран со скрытым контентом"
+    } else {
+        ""
+    }
+    println(output)
+}
