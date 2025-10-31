e2098722fa5b0ea202c78928b9f3b3e3690a7ed4
tutyperefruty <tutyperefruty@gmail.com>
2025-10-31 09:53:37 +0900

    KS-4-2 refactor

--- src/main/kotlin/lesson_4/lesson4_task2.kt
+++ src/main/kotlin/lesson_4/lesson4_task2.kt
@@ -1,5 +1,9 @@
 package org.example.lesson_4
 
+const val WEIGHT_MIN = 35
+const val WEIGHT_MAX = 100
+const val VOLUME_CRIT = 100
+
 fun main() {
 
     val weightA = 20
@@ -7,7 +11,7 @@ fun main() {
     val weightB = 50
     val litersB = 100
 
-    println("A: The cargo with a weight of $weightA kg and a volume of $litersA liters corresponds to the category \'Average\': ${(weightA > 35 && weightA <= 100) and (litersA < 100)}")
-    println("B: The cargo with a weight of $weightB kg and a volume of $litersB liters corresponds to the category \'Average\': ${(weightB > 35 && weightB <= 100) and (litersB < 100)}")
+    println("A: The cargo with a weight of $weightA kg and a volume of $litersA liters corresponds to the category \'Average\': ${(weightA > WEIGHT_MIN && weightA <= WEIGHT_MAX) and (litersA < VOLUME_CRIT)}")
+    println("B: The cargo with a weight of $weightB kg and a volume of $litersB liters corresponds to the category \'Average\': ${(weightB > WEIGHT_MIN && weightB <= WEIGHT_MAX) and (litersB < VOLUME_CRIT)}")
 
 }
