7ff68f12a3d843b07a28fcc054304e19dfe7d7b6
tutyperefruty <tutyperefruty@gmail.com>
2025-11-01 20:43:55 +0900

    KS-5-5 lotto autogenerate

---
+++ src/main/kotlin/lesson_5/lesson5_task5.kt
@@ -0,0 +1,32 @@
+package org.example.lesson_5
+
+import kotlin.random.Random
+
+fun main() {
+    val firstNum = Random.nextInt(0, 5)
+    val secondNum = Random.nextInt(0, 5)
+    val thirdNum = Random.nextInt(0, 5)
+    val lottoList = listOf(firstNum, secondNum, thirdNum)
+//    println(lottoList)
+
+    println("\nInter three numbers from 0 up to 42 to check luck!\nFirst number: ")
+    val firstInput = readln().toInt()
+    println("Second number: ")
+    val secondInput = readln().toInt()
+    println("Third number: ")
+    val thirdInput = readln().toInt()
+    val myList = listOf(firstInput, secondInput, thirdInput)
+
+    val crossNums = lottoList.intersect(myList).size
+    println(crossNums)
+
+    val result = when (crossNums) {
+        1 -> "Получаешь утешительный приз"
+        2 -> "Получаешь скромный приз"
+        3 -> "Джекпот!"
+        else -> "Nothing matches"
+    }
+    println(result)
+    println("Правильные данные: ${firstNum}, ${secondNum}, ${thirdNum}.")
+
+}
