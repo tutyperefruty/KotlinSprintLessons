224a6ea045fad148ebefdfc64d6d173864568005
tutyperefruty <tutyperefruty@gmail.com>
2025-11-01 11:22:18 +0900

    KS-5-3 refactor

--- src/main/kotlin/lesson_5/lesson5_task3.kt
+++ src/main/kotlin/lesson_5/lesson5_task3.kt
@@ -1,22 +1,28 @@
 package org.example.lesson_5
 
+const val MIN_VALUE = 0
+const val MAX_VALUE = 42
+
 fun main() {
 
     val firstNum: Int = 3
     val secondNum: Int = 9
 
-    println("Введите первое число от 0 до 42: ")
-    val firstInput = readLine()!!.toInt()
-    println("Введите второе число от 0 до 42: ")
-    val secondInput = readLine()!!.toInt()
+    println("Введите первое число от ${MIN_VALUE} до ${MAX_VALUE}: ")
+    val firstInput = readln().toInt()
+    println("Введите второе число от ${MIN_VALUE} до ${MAX_VALUE}: ")
+    val secondInput = readln().toInt()
 
-    val result = when {
-        (firstInput == firstNum) or (firstInput == secondNum) && (secondInput == secondNum) or (secondInput == firstNum) -> "Поздравляем! Вы выиграли главный приз!"
-        (firstInput == firstNum) or (firstInput == secondNum) or (secondInput == secondNum) or (secondInput == firstNum) -> "Вы выиграли утешительный приз!"
-        else -> "Неудача!"
-    }
+    val result =
+        if (((firstInput == firstNum) || (firstInput == secondNum)) && ((secondInput == secondNum) || (secondInput == firstNum))) {
+            "Поздравляем! Вы выиграли главный приз!"
+        } else if (((firstInput == firstNum) || (firstInput == secondNum)) or ((secondInput == secondNum) || (secondInput == firstNum))) {
+            "Вы выиграли утешительный приз!"
+        } else {
+            "Неудача!"
+        }
 
     println(result)
-    println("Правильные числа: ${firstNum} и ${secondNum}")
+    println("Правильные числа: ${secondNum} и ${firstNum}")
 
 }
