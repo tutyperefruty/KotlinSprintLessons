4cb85a5fcca427ad767b98286f0fcfab22432a66
tutyperefruty <tutyperefruty@gmail.com>
2025-11-01 10:40:33 +0900

    KS-5-1 refactor

--- src/main/kotlin/lesson_5/lesson5-task1.kt
+++ src/main/kotlin/lesson_5/lesson5-task1.kt
@@ -1,20 +1,20 @@
 package org.example.lesson_5
 
-const val OK_MSG = "Добро пожаловать!"
-const val NOT_OK_MSG = "Доступ запрещен."
+const val FIRST_NUM = 3
+const val SECOND_NUM = 6
 
 fun main() {
 
-    print("Введи(пример: 1+2=3) и реши простой математический пример – сложи два числа: ")
-    val input = readLine()!!.split("+", "=")
-    val part1 = input[0].toInt()
-    val part2 = input[1].toInt()
-    val part3 = input[2].toInt()
+    print("Введи результат суммы двух чисел: ${FIRST_NUM} + ${SECOND_NUM} = ")
+    val input = readln()?.toInt()
 
-    val result = if (part3 == part2 + part1) {
-        OK_MSG
+    val okMsg = "Добро пожаловать!"
+    val notOkMsg = "Доступ запрещен."
+
+    val result = if (input == FIRST_NUM + SECOND_NUM) {
+        okMsg
     } else {
-        NOT_OK_MSG
+        notOkMsg
     }
     println(result)
 }
