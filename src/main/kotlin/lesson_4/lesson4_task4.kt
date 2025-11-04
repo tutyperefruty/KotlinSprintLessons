b5314489f4e87cadbb47f4b00812c2fd0675cfbe
tutyperefruty <tutyperefruty@gmail.com>
2025-11-04 16:16:24 +0900

    KS-4-4 refactor

--- src/main/kotlin/lesson_4/lesson4_task4.kt
+++ src/main/kotlin/lesson_4/lesson4_task4.kt
@@ -2,23 +2,19 @@ package org.example.lesson_4
 
 fun main() {
 
-    val handsarms = 1
-    val abs = 1
-    val legs = 0
-    val back = 0
+    val handsArmsAbs = 1
 
     val todayIsDay = 5
 
-    val handArmsRemainder = todayIsDay % 2 == handsarms
-    val absRemainder = handArmsRemainder
-    val legsRemainder = !handArmsRemainder
-    val backRemainder = !handArmsRemainder
+    val handArmsAbsRemainder = todayIsDay % 2 == handsArmsAbs
 
-    println("""
-        Exercises for hands: $handArmsRemainder
-        Exercises for abs:   $absRemainder
-        Exercises for legs:  $legsRemainder
-        Exercises for back:  $backRemainder
-    """.trimIndent())
+    println(
+        """
+        Exercises for hands: $handArmsAbsRemainder
+        Exercises for abs:   $handArmsAbsRemainder
+        Exercises for legs:  ${!handArmsAbsRemainder}
+        Exercises for back:  ${!handArmsAbsRemainder}
+    """.trimIndent()
+    )
 
 }
