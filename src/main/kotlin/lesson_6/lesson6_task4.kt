a3f42f3299358177321ada94838b1a3223190d9f
tutyperefruty <tutyperefruty@gmail.com>
2025-11-10 19:00:05 +0900

    KS-6-4 game from 1 to 9

---
+++ src/main/kotlin/lesson_6/lesson6_task4.kt
@@ -0,0 +1,25 @@
+package org.example.lesson_6
+
+const val MIN_NUM = 1
+const val MAX_NUM = 9
+
+fun main() {
+
+    var attempt: Int = 5
+    val ranList = ((MIN_NUM)..(MAX_NUM)).random()
+
+    do {
+        println("\nInput any one number from 1 to 9: ")
+        val inputNum = readln().toInt()
+
+        if (inputNum == ranList) {
+            println("It was a great game")
+            break
+        } else {
+            attempt--
+            println("Wrong, ostatok popitok ${attempt}")
+            if (attempt == 0) println("It was number ${ranList}")
+        }
+    } while (attempt > 0)
+
+}
