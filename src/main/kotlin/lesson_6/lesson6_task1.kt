54290f349df636c2b42cdd511031ca23c947dff4
tutyperefruty <tutyperefruty@gmail.com>
2025-11-12 22:24:53 +0900

    KS-6-1 refactor (create ID PASS 1time)

--- src/main/kotlin/lesson_6/lesson6_task1.kt
+++ src/main/kotlin/lesson_6/lesson6_task1.kt
@@ -2,17 +2,17 @@ package org.example.lesson_6
 
 fun main() {
 
-    do {
         println("New ID: ")
         val newID = readln()
         println("New PASSWORD: ")
         val newPass = readln()
 
+    do {
         println("Input your ID: ")
         val yourID = readln()
         println("Input your PASSWORD: ")
         val yourPass = readln()
-    } while (newID != yourID && newPass != yourPass)
+    } while ((newID != yourID) or (newPass != yourPass))
 
     println("Авторизация прошла успешно.")
 
