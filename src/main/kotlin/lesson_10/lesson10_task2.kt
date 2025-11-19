1f0aed697544609e7f554fbe1670394a603cd27d
tutyperefruty <tutyperefruty@gmail.com>
2025-11-19 23:10:25 +0900

    KS-10-2 refactor

--- src/main/kotlin/lesson_10/lesson10_task2.kt
+++ src/main/kotlin/lesson_10/lesson10_task2.kt
@@ -8,7 +8,7 @@ fun main() {
     val newLogin = readln()
     println("Input new password: ")
     val newPass = readln()
-    if ((validateLoginAndPasswordLength(vhodnieDannie1 = newLogin, vhodnieDannie2 = newPass)) == true) {
+    if ((validateLoginAndPasswordLength(newLogin, newPass)) == true) {
         println("Glad to greet you!")
     } else {
         println("Login or Password is not rather long")
@@ -16,4 +16,4 @@ fun main() {
 }
 
 fun validateLoginAndPasswordLength(vhodnieDannie1: String, vhodnieDannie2: String): Boolean =
-    ((vhodnieDannie1.length >= MIN_LENGTH) and (vhodnieDannie2.length >= MIN_LENGTH))
+    ((vhodnieDannie1.length >= MIN_LENGTH) && (vhodnieDannie2.length >= MIN_LENGTH))
