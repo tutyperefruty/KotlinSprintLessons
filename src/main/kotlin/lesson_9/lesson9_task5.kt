188148363521f1c5a10c8325b5aa18d8cb03305e
tutyperefruty <tutyperefruty@gmail.com>
2025-11-19 16:33:07 +0900

    KS-9-5 refactor (toSet)

--- src/main/kotlin/lesson_9/lesson9_task5.kt
+++ src/main/kotlin/lesson_9/lesson9_task5.kt
@@ -9,18 +9,10 @@ fun main() {
     val ingredientList = MutableList(INGREDIENT_NUMS) { "" }
     for (ingredient in ingredientList.indices) {
         println("Enter ingredient: ")
-        val input = readln()
-        if (ingredientList.contains(input)) {
-            println("Don't dublicate, reenter, pls!: ")
-            val reInput = readln()
-            ingredientList[ingredient] = reInput
-        } else {
-            ingredientList[ingredient] = input
-        }
+            ingredientList[ingredient] = readln()
     }
-
-    val conditionedList = ingredientList.sorted().toMutableList()
-    conditionedList[0] = conditionedList[0].replaceFirstChar { it.uppercase(Locale.getDefault()) }
+//
+    val conditionedList = ingredientList.sorted().toSet().toMutableList()
+    conditionedList[0] = conditionedList[0].replaceFirstChar { it.titlecase(Locale.getDefault()) }
     println(conditionedList.joinToString(","))
-
 }
