5dc3c3ff79d9374fc628b3956fb42aedd1d2b0e3
tutyperefruty <tutyperefruty@gmail.com>
2025-11-24 04:00:41 +0900

    KS-11-3 ROOM with Member_List

---
+++ src/main/kotlin/lesson_11/lesson11_task3.kt
@@ -0,0 +1,49 @@
+package org.example.lesson_11
+
+class Member(
+    var name: String = "...empty Nik...",
+    var status: String = mutableListOf("SPEAKING", "MIC_OFF", "MUTED").random(),
+    var avatar: String = "deafult_IMG.png",
+) {
+    fun updateStatus(newStatus: String) {
+        status = newStatus
+    }
+}
+
+class Room(
+    var title: String = "...no Title...",
+    var coverImage: String = "...no Cover...",
+    var membersList: MutableList<Member> = mutableListOf(),
+) {
+    fun addMember(himOrHer: Member) {
+        membersList.add(himOrHer)
+    }
+
+    fun updateMemberStatusFindByName(name: String, newStatus: String) {
+        val findMember: Member? = membersList.find { it.name == name }
+        findMember?.updateStatus(newStatus)
+    }
+
+    fun printMemberList() {
+        membersList.forEach {
+            println("(Avatar: ${it.avatar}) --- on long click---> Shows nik: ${it.name} --and shows--> Status: ${it.status}")
+        }
+    }
+}
+
+fun main() {
+    val member_1: Member = Member(name = "Alya", avatar = "avatar1.png")
+    val member_2: Member = Member(name = "Balya", avatar = "avatar2.png")
+    val member_3: Member = Member()
+
+    val roomForSpeaking: Room = Room("For Speaking", "room_cover.png")
+
+    roomForSpeaking.addMember(member_1)
+    roomForSpeaking.addMember(member_2)
+    roomForSpeaking.addMember(member_3)
+
+    roomForSpeaking.updateMemberStatusFindByName("Alya", "SPEAKING")
+
+    println("(Cover: ${roomForSpeaking.coverImage}) - Room: ${roomForSpeaking.title}")
+    roomForSpeaking.printMemberList()
+}                                                                                                                                            
