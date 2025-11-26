0a1613666fab0eb531cc9160e87302b9b635e6f5
tutyperefruty <tutyperefruty@gmail.com>
2025-11-26 19:20:47 +0900

    KS-12-1 CLASS two_from EMPTY constructor()

---
+++ src/main/kotlin/lesson_12/lesson12_task1.kt
@@ -0,0 +1,26 @@
+package org.example.lesson_12
+
+class OneDayWeather constructor() {
+    var dayTemper: Int = 10
+    var nightTemper: Int = 5
+    var precipitation: Boolean = false
+
+    fun printByOne() {
+        return println("Day temperature is: ${dayTemper}°C, Night temperature is: ${nightTemper}°C, Precipitation is: ${if (precipitation) "Yes" else "No"}")
+    }
+
+}
+
+fun main() {
+
+    val day1: OneDayWeather = OneDayWeather()
+    day1.dayTemper = 20
+    day1.nightTemper = 15
+    val day2: OneDayWeather = OneDayWeather()
+    day2.dayTemper = 30
+    day2.nightTemper = 25
+    day2.precipitation = true
+
+    println("${day1.printByOne()}\n${day2.printByOne()}")
+
+}
