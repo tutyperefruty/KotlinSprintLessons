d229aa6e0ce7973d5ebe48dab04ab9e3ae9b642e
tutyperefruty <tutyperefruty@gmail.com>
2025-11-28 20:06:41 +0900

    KS-13-2 refactor3

--- src/main/kotlin/lesson_13/lesson13_task2.kt
+++ src/main/kotlin/lesson_13/lesson13_task2.kt
@@ -1,9 +1,7 @@
 package org.example.lesson_13
 
 class TelBookContact(
-    val name: String,
-    val number: Long,
-    val company: String? = null
+    val name: String, val number: Long, val company: String? = null
 ) {
     fun printInfo() {
         println("- Name: $name\n- Number: $number\n- Company: ${company ?: "Not specified"}")
@@ -11,6 +9,6 @@ class TelBookContact(
 }
 
 fun main() {
-    val contact1: TelBook = TelBook("Alya", 9876543210, "Gug")
+    val contact1: TelBookContact = TelBookContact("Alya", 9876543210, "Gug")
     contact1.printInfo()
 }
