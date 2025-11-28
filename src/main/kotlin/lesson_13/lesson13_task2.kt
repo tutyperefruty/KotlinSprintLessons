7e9305309fdf6100d45684f06fe89e44f3cc66c5
tutyperefruty <tutyperefruty@gmail.com>
2025-11-28 16:01:44 +0900

    KS-13-1 TelBook company_null

---
+++ src/main/kotlin/lesson_13/lesson13_task2.kt
@@ -0,0 +1,25 @@
+package org.example.lesson_13
+
+class TelBook(val name: String, val number: Long,) {
+
+    var company: String = "Not specified"
+
+    constructor(
+        name: String,
+        number: Long,
+        companyInput: String? = null,
+    ) : this(name, number) {
+        if (companyInput == null) company else this.company = companyInput
+    }
+
+    fun printInfo() {
+        println("- Name: $name\n- Number: $number\n- Company: $company")
+    }
+}
+
+fun main() {
+
+    val contact1: TelBook = TelBook("Alya", 9876543210, "Gugyyyy")
+    contact1.printInfo()
+
+}
