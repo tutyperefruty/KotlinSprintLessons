5a664d7e3b468b652c6613169703de67d77911bd
tutyperefruty <tutyperefruty@gmail.com>
2025-11-28 20:00:38 +0900

    KS-13-2 refactor

--- src/main/kotlin/lesson_13/lesson13_task2.kt
+++ src/main/kotlin/lesson_13/lesson13_task2.kt
@@ -1,24 +1,11 @@
 package org.example.lesson_13
 
-class TelBook(val name: String, val number: Long,) {
-
-    var company: String = "Not specified"
-
-    constructor(
-        name: String,
-        number: Long,
-        companyInput: String? = null,
-    ) : this(name, number) {
-        if (companyInput == null) company else this.company = companyInput
-    }
-
+class TelBookContact(
+    val name: String,
+    val number: Long,
+    val company: String? = null
+) {
     fun printInfo() {
-        println("- Name: $name\n- Number: $number\n- Company: $company")
+        println("- Name: $name\n- Number: $number\n- Company: ${company ?: "Not specified"}")
     }
-}
-
-fun main() {
-    val contact1: TelBook = TelBook("Alya", 9876543210, "Gugyyyy")
-    contact1.printInfo()
-
 }
