8ec825951b6e5be227e3bc7753ca962896177d34
tutyperefruty <tutyperefruty@gmail.com>
2025-12-01 05:59:30 +0900

    KS-13-4 refactor

--- src/main/kotlin/lesson_13/lesson13_task4.kt
+++ src/main/kotlin/lesson_13/lesson13_task4.kt
@@ -1,32 +1,42 @@
 package org.example.lesson_13
 
-class TelBookContacts(val name: String, val number: Long?, val company: String?) {
+class Contact(val name: String, val phoneNumber: Long, val company: String?) {
     fun getContactInfo() {
-        println("Your contact is: Name - $name, Phone number - $number, Company - $company.")
+        println("Next contact was created: Name - $name, Phone number - $phoneNumber, Company - $company.")
     }
 }
 
 fun main() {
+    val contactsList = mutableListOf<Contact>()
 
-    println("Enter your Name: ")
-    val inputName = readln()
+    while (true) {
+        println("Enter your Name: ")
+        val inputName = readln()
 
-    println("Enter your Phone number: ")
-    val inputPhoneNumber = readln()
-    if (inputPhoneNumber.isEmpty()) {
-        println("Phone number must be specified!")
-        return
+        println("Enter your Phone number: ")
+        val inputPhoneNumber: Long = readln().toLongOrNull().let {
+            if (it == null) {
+                println("Phone number must be specified.")
+                continue
+            } else {
+                it
+            }
+        }
+
+        println("Enter your Company: ")
+        val inputCompany = readln().let { if (it.isEmpty()) null else it }
+
+        val contact = Contact(inputName, inputPhoneNumber, inputCompany)
+        contactsList.add(contact)
+
+        contact.getContactInfo()
+
+        println("Would you like to add one more contact?")
+        val continueOrNot = readln().lowercase().let { if (it != "yes") break }
+
     }
 
-    println("Enter your Company: ")
-    var inputCompany: String? = readln()
-    if (inputCompany!!.isEmpty()) {
-        inputCompany = null
-    }
-
-    val contacts: MutableList<TelBookContacts> = mutableListOf()
-    val newContact: TelBookContacts = TelBookContacts(inputName, inputPhoneNumber.toLongOrNull(), inputCompany.toString())
-    contacts.add(newContact)
-    newContact.getContactInfo()
-
+    var firstContact: Int = 1
+    println("Full list of Contacts:")
+    contactsList.forEach { println("Contact ${firstContact++}: ${it.name}, ${it.phoneNumber}, ${it.company}.") }
 }
