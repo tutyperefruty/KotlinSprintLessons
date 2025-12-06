bf5f739f557a1078a8f1895771d8fb6b443959d8
tutyperefruty <tutyperefruty@gmail.com>
2025-12-07 01:43:23 +0900

    KS-15-1 refactor 2

--- src/main/kotlin/lesson_15/lesson15_task1.kt
+++ src/main/kotlin/lesson_15/lesson15_task1.kt
@@ -1,31 +1,25 @@
 package org.example.lesson_15
 
 interface Swimmable {
-    fun canSwim() = println("Can swim")
+    val name: String
+    fun canSwim() = println("$name swims")
 }
 
 interface Flyable {
-    fun canFly() = println("Can fly")
+    val name: String
+    fun canFly() = println("$name flies")
 }
 
-abstract class Entity : Swimmable, Flyable {
-    abstract val name: String
-    override fun canSwim() = println("$name swims")
-    override fun canFly() = println("$name flies")
-}
+class Carp(override val name: String) : Swimmable
 
-class Carp(override val name: String) : Entity(), Swimmable
+class Gull(override val name: String) : Swimmable, Flyable
 
-class Gull(override val name: String) : Entity(), Swimmable, Flyable
-
-class Duck(override val name: String) : Entity(), Swimmable, Flyable
+class Duck(override val name: String) : Swimmable, Flyable
 
 fun main() {
 
-    val entieties: MutableList<Entity> = mutableListOf()
-    with(entieties) {
-        Carp("Carp-01").apply { canSwim() }
-        Gull("Gull-01").apply { canSwim(); canFly() }
-        Duck("Duck-01").apply { canFly(); canSwim() }
-    }
+    val carp1 = Carp("Carp-01").run { canSwim() }
+    val gull1 = Gull("Gull-01").run { canSwim(); canFly() }
+    val duck1 = Duck("Duck-01").run { canFly(); canSwim() }
+
 }
