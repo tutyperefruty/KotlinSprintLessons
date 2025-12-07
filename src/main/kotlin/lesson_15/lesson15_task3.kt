b91c0e65aa1db47e447a50fab33844fc4e7c2f50
tutyperefruty <tutyperefruty@gmail.com>
2025-12-08 05:39:12 +0900

    KS-15-3 refactor

--- src/main/kotlin/lesson_15/lesson15_task3.kt
+++ src/main/kotlin/lesson_15/lesson15_task3.kt
@@ -1,36 +1,54 @@
 package org.example.lesson_15
 
 abstract class User(val name: String) {
+
     fun readForum() = println("$name is reading a forum")
-    fun writeMessage(msgForInput: String) = println("$name is writing a message '$msgForInput'")
+
+    fun writeMessage(message: String) = println("$name is writing a message '$message'")
+
+    abstract fun showActions()
 }
 
 class Administrator(name: String) : User(name) {
+
+    override fun showActions() {
+        println("Admin can read the forum, write and delete messages, and delete users")
+    }
+
     fun deleteUser(user: User) {
         println("$name is deleting user: ${user.name}")
     }
 
-    fun deleteMessage(msgForDelete: String) {
-        println("$name is deleting '${msgForDelete}'")
+    fun deleteMessage(message: String) {
+        println("$name is deleting '${message}'")
     }
+
 }
 
-class RegularUser(name: String) : User(name)
+class RegularUser(name: String) : User(name) {
+
+    override fun showActions() {
+        println("Regular Users can read the forum and write messages")
+    }
+
+}
 
 fun main() {
 
     val regularUser = RegularUser("Axel")
     val admin = Administrator("Mr.Admin")
 
-    val printTest = with(regularUser) {
+    with(regularUser) {
+        showActions()
         readForum()
         writeMessage("I'm Regularlarlar")
-    }.also {
-        with(admin) {
-            readForum()
-            writeMessage("I'm Adminminmin")
-            deleteMessage("Wow")
-            deleteUser(regularUser)
-        }
     }
+    with(admin) {
+        showActions()
+        readForum()
+        writeMessage("I'm Adminminmin")
+        deleteMessage("Wow")
+        deleteUser(regularUser)
+    }
+
 }
