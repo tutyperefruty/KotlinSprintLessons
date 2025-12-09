d243ec11c38332f45b3e547d65bd9898aefe8520
tutyperefruty <tutyperefruty@gmail.com>
2025-12-09 20:47:58 +0900

    KS-16-2 Dice

---
+++ src/main/kotlin/lesson_16/lesson16_task2.kt
@@ -0,0 +1,18 @@
+package org.example.lesson_16
+
+class Circle(private val radius: Double) {
+
+    private val Pi = 3.14
+
+    fun circumference() = println("Circumference of Circle with radius ${radius} is ${"%.1f".format(2 * Pi * radius)}")
+
+    fun area() = println("Area of Circle with radius ${radius} is ${"%.1f".format(Pi * Pi * radius)}")
+}
+
+fun main() {
+
+    val circle = Circle(5.0).apply {
+        circumference()
+        area()
+    }
+}
