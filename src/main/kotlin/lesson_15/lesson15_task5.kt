4e7998e63cbbff891032eee39e10755a87bd6242
tutyperefruty <tutyperefruty@gmail.com>
2025-12-09 10:41:30 +0900

    KS-15-5 (RENEW) Interface, class Truck and Car

--- src/main/kotlin/lesson_15/lesson15_task5.kt
+++ src/main/kotlin/lesson_15/lesson15_task5.kt
@@ -1,59 +1,119 @@
 package org.example.lesson_15
 
-interface Movable {
-    val name: String
-    fun startsMove() {
-        println("$name starts move.")
+const val NAME = "01"
+const val CURRENT = 0
+const val MAX_PASS_TRUCK = 1
+const val MAX_CARGO_TRUCK = 2
+const val MAX_PASS_CAR = 3
+
+interface Transportable {
+    fun move()
+    fun moveBack()
+}
+
+interface PassengerTransport : Transportable {
+    fun loadPassengers(value: Int? = null)
+    fun unloadPassengers()
+}
+
+interface CargoTransport : Transportable {
+    fun loadCargo(value: Int? = null)
+    fun unloadCargo()
+}
+
+class Truck(
+    private val name: String = NAME,
+    private var currentPassengers: Int = CURRENT,
+    private var currentCargo: Int = CURRENT
+) : PassengerTransport, CargoTransport {
+
+    private var maxPassengers = MAX_PASS_TRUCK
+    private var maxCargo = MAX_CARGO_TRUCK
+
+    override fun move() = println("Truck-$name moves")
+
+    override fun moveBack() = println("Truck-$name returns")
+
+    override fun loadPassengers(value: Int?) {
+        if (value != null) {
+            this.maxPassengers = value
+        }
+        while (currentPassengers < maxPassengers) {
+            currentPassengers++
+            println("1 Passenger was loaded to truck-$name")
+        }
+    }
+
+    override fun unloadPassengers() {
+        while (currentPassengers > CURRENT) {
+            currentPassengers--
+            println("1 Passenger was unloaded from truck-$name")
+        }
+    }
+
+    override fun loadCargo(value: Int?) {
+        if (value != null) {
+            this.maxCargo = value
+        }
+        while (currentCargo < maxCargo) {
+            currentCargo++
+            println("Loaded 1 ton to truck-$name")
+        }
+    }
+
+    override fun unloadCargo() {
+        while (currentCargo > CURRENT) {
+            currentCargo--
+            println("Unloaded 1 ton from truck-$name")
+        }
     }
 }
 
-interface PassengerTransportable {
-    val name: String
-    val passengers: Int
+class Car(private val name: String = NAME, private var currentPassengers: Int = CURRENT) : PassengerTransport {
 
-    fun transportsPassenger() {
-        println("$name transports $passengers passenger(s).")
+    private var maxPassengers = MAX_PASS_CAR
+
+    override fun move() = println("Car-$name moves")
+
+    override fun moveBack() = println("Car-$name returns")
+
+    override fun loadPassengers(value: Int?) {
+        if (value != null) {
+            this.maxPassengers = value
+        }
+        while (currentPassengers < maxPassengers) {
+            currentPassengers++
+            println("1 Passenger was loaded to Car-$name")
+        }
+    }
+
+    override fun unloadPassengers() {
+        while (currentPassengers > CURRENT) {
+            currentPassengers--
+            println("1 Passenger was unloaded from car-$name")
+        }
     }
 }
-
-interface CargoTransportable {
-    val name: String
-    val passengers: Int
-    val cargo: Int
-
-    fun transportsPassenger() {
-        println("$name transports $passengers passenger(s).")
-    }
-
-    fun transportsCargo() {
-        println("$name transports cargo in the amount of $cargo ton(s)")
-    }
-}
-
-class PassengerCar(override val name: String = "PassengerCar", override val passengers: Int = 3) : Movable,
-    PassengerTransportable
-
-class Truck(override val name: String = "Truck", override val passengers: Int = 1, override val cargo: Int = 2) :
-    Movable,
-    CargoTransportable
 
 fun main() {
 
-    val truck1 = Truck()
-    val car1 = PassengerCar()
-    truck1.apply {
-        startsMove()
-        transportsPassenger()
-        transportsCargo()
-    }
-    car1.apply {
-        startsMove()
-        transportsPassenger()
-    }
-    val car2 = PassengerCar("PassengerCar-02", 2)
-    car2.apply {
-        startsMove()
-        transportsPassenger()
-    }
+    val truck01: Truck = Truck()
+    val car01: Car = Car()
 
+    truck01.apply {
+        loadCargo()
+        loadPassengers()
+        move()
+        unloadPassengers()
+        unloadCargo()
+    }
+    car01.apply {
+        loadPassengers()
+        move()
+        unloadPassengers()
+        moveBack()
+        loadPassengers(2)
+        move()
+        unloadPassengers()
+    }
 }
