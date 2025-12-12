63243f00b8c8329010df44d2b2474cb296fefce9
tutyperefruty <tutyperefruty@gmail.com>
2025-12-13 05:55:43 +0900

    KS-16-2 Radius

--- src/main/kotlin/lesson_16/lesson16_task2.kt
+++ src/main/kotlin/lesson_16/lesson16_task2.kt
@@ -3,15 +3,13 @@ package org.example.lesson_16
 class Circle(private val radius: Double) {
 
     private val Pi = 3.14
-
     fun circumference() = println("Circumference of Circle with radius ${radius} is ${"%.1f".format(2 * Pi * radius)}")
-
     fun area() = println("Area of Circle with radius ${radius} is ${"%.1f".format(Pi * Pi * radius)}")
 }
 
 fun main() {
 
-    val circle = Circle(5.0).apply {
+    with(Circle(5.0)) {
         circumference()
         area()
     }
