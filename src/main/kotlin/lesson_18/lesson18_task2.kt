63dbf31c45fae1545b5d9475e4d173a3b0e8adcc
tutyperefruty <tutyperefruty@gmail.com>
2025-12-16 22:14:27 +0900

    KS-18-2 refactor

--- src/main/kotlin/lesson_18/lesson18_task2.kt
+++ src/main/kotlin/lesson_18/lesson18_task2.kt
@@ -2,50 +2,30 @@ package org.example.lesson_18
 
 const val FROM = 1
 
-open class Dice {
-
-    open fun rollDice() = ""
-
+abstract class Dice {
+    abstract val sides: Int
+    open fun rollDice() = println("${(FROM..sides).random()}")
 }
 
 class Dice4 : Dice() {
-
-    private val sides: Int = 4
-    private val sideValue: Int = (FROM..sides).random()
-
-    override fun rollDice() = "$sideValue"
-
+    override val sides: Int = 4
 }
 
 class Dice6 : Dice() {
-
-    private val sides: Int = 6
-    private val sideValue: Int = (FROM..sides).random()
-
-    override fun rollDice() = "$sideValue"
-
+    override val sides: Int = 6
 }
 
 class Dice8 : Dice() {
-
-    private val sides: Int = 8
-    private val sideValue: Int = (FROM..sides).random()
-
-    override fun rollDice() = "$sideValue"
-
+    override val sides: Int = 8
 }
 
-
 fun main() {
-
     val dice4: Dice = Dice4()
     val dice6: Dice = Dice6()
     val dice8: Dice = Dice8()
 
-    val sides: Array<Dice> = arrayOf(dice4, dice6, dice8)
-    sides.forEach { side ->
-        println(side.rollDice())
+    val dices: Array<Dice> = arrayOf(dice4, dice6, dice8)
+    dices.forEach {
+        it.rollDice()
     }
-
-
 }
