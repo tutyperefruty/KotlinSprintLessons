4f921883109c77b14fa2cb0bc7fb7d30ae5d28eb
tutyperefruty <tutyperefruty@gmail.com>
2025-12-18 03:14:13 +0900

    KS-18-5 refactor

--- src/main/kotlin/lesson_18/lesson18_task5.kt
+++ src/main/kotlin/lesson_18/lesson18_task5.kt
@@ -1,43 +1,30 @@
 package org.example.lesson_18
 
-const val WAS_DRAWN_IN = "was drawn in coordinates"
-
-enum class Figure {
-    SQUARE,
-    CIRCLE,
-    POINT
-}
-
-fun showForInt(figure: Figure, x: Int, y: Int) = println("${figure.name} $WAS_DRAWN_IN ($x, $y)")
-fun showForInt(figure: Figure, x: Float, y: Float) = println("${figure.name} $WAS_DRAWN_IN ($x, $y)")
+const val WAS_DROWN_IN = "was drawn in coordinates"
 
 class Screen {
-    fun draw(figure: Figure, x: Int, y: Int): String {
-        when (figure) {
-            Figure.SQUARE -> showForInt(figure, x, y)
-            Figure.CIRCLE -> showForInt(figure, x, y)
-            Figure.POINT -> showForInt(figure, x, y)
-        }
-        return ""
-    }
-    fun draw(figure: Figure, x: Float, y: Float): String {
-        when (figure) {
-            Figure.SQUARE -> showForInt(figure, x, y)
-            Figure.CIRCLE -> showForInt(figure, x, y)
-            Figure.POINT -> showForInt(figure, x, y)
-        }
-        return ""
-    }
+    fun drawSquare(name: String, x: Int, y: Int) = println(showInfo(name, x, y))
+    fun drawSquare(name: String, x: Float, y: Float) = println(showInfo(name, x, y))
+
+    fun drawCircle(name: String, x: Int, y: Int) = println(showInfo(name, x, y))
+    fun drawCircle(name: String, x: Float, y: Float) = println(showInfo(name, x, y))
+
+    fun drawPoint(name: String, x: Int, y: Int) = println(showInfo(name, x, y))
+    fun drawPoint(name: String, x: Float, y: Float) = println(showInfo(name, x, y))
 }
+
+fun showInfo(name: String, x: Any, y: Any) = "$name $WAS_DROWN_IN x = $x, y = $y"
 
 fun main() {
     val myScreen: Screen = Screen()
     with(myScreen) {
-        draw(Figure.SQUARE, 2, 3)
-        draw(Figure.CIRCLE, 3, 4)
-        draw(Figure.POINT, 1, 0)
-        draw(Figure.SQUARE, 2.3f, 3.5f)
-        draw(Figure.CIRCLE, 3.9f, 1.3f)
-        draw(Figure.POINT, 5.1f, 7.3f)
+        drawSquare("Square", 2, 3)
+        drawSquare("Square", 2.3f, 3.5f)
+        println()
+        drawCircle("Circle", 3, 4)
+        drawCircle("Circle", 3.9f, 1.3f)
+        println()
+        drawPoint("Point",1, 0)
+        drawPoint("Point", 5.1f, 7.3f)
     }
 }
