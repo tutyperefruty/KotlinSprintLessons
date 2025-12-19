86c301e6deb32916fec375e31d255dee6fa9816d
tutyperefruty <tutyperefruty@gmail.com>
2025-12-19 23:48:36 +0900

    KS-19-5 refactor

--- src/main/kotlin/lesson_19/lesson19_task5.kt
+++ src/main/kotlin/lesson_19/lesson19_task5.kt
@@ -1,18 +1,12 @@
 package org.example.lesson_19
 
 const val LISTING_SIZE = 5
-const val MALE = "male"
-const val FEMALE = "female"
 
-enum class Sex {
-    MALE,
-    FEMALE,
-    UNKNOWN
-}
+enum class Sex { MALE, FEMALE }
 
 class PersonFile(
     private val name: String,
-    private val sex: Sex = Sex.UNKNOWN
+    private val sex: Sex?
 ) {
     companion object {
         fun displayFilesList(personFilesList: MutableList<PersonFile>) {
@@ -30,17 +24,17 @@ fun main() {
 
     while (personFilesList.size < LISTING_SIZE) {
         println("Enter Name how you want. Blank does mean 'No-named':")
-        val inputName: String = readln().ifBlank {"NO_NAME"}
+        val inputName: String = readln().ifBlank { "NO_NAME" }
 
         println(
             "Enter the gender [Instructions: Enter only in English in all cases, e.g., mAle, FemalE. " +
-                    "If you enter anything else or nothing, your sex will be set as 'UNKNOWN']: "
+                    "If you enter anything else or nothing, your sex will be set as 'null']: "
         )
         val inputSex: String = readln().lowercase()
         val sex = when (inputSex) {
-            MALE -> Sex.MALE
-            FEMALE -> Sex.FEMALE
-            else -> Sex.UNKNOWN
+            Sex.MALE.name.lowercase() -> Sex.MALE
+            Sex.FEMALE.name.lowercase() -> Sex.FEMALE
+            else -> null
         }
 
         personFilesList.add(PersonFile(name = inputName, sex = sex))
