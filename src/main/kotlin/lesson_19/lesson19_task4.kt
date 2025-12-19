fa3cdd6656ea19631f9b55ac8d45d651ce10a7e6
tutyperefruty <tutyperefruty@gmail.com>
2025-12-20 06:44:26 +0900

    KS-19-4 refactor

--- src/main/kotlin/lesson_19/lesson19_task4.kt
+++ src/main/kotlin/lesson_19/lesson19_task4.kt
@@ -5,27 +5,28 @@ enum class Bullet(val power: Int) { BLUE(5), GREEN(10), RED(20) }
 class Tank {
     private var loadedBullet: Bullet? = null
 
-    fun chargeBullet(bullet: Bullet? = null) {
+    fun chargeBullet(bullet: Bullet) {
         loadedBullet = bullet
         println("Got $loadedBullet bullet")
     }
 
-    fun shoot() = loadedBullet?.power?.let { println("Shoooot! Damage -$it") } ?: println("Can't shoot by nothing!")
+    fun shoot() {
+        loadedBullet?.power?.let { println("Shoooot! Damage -$it") } ?: println("Can't shoot by nothing!")
+        loadedBullet = null
+    }
 }
 
 fun main() {
     val tank = Tank()
     with(tank) {
         println()
-        chargeBullet()
-        Thread.sleep(1500L)
-        shoot()
-        Thread.sleep(1500L)
         chargeBullet(Bullet.BLUE)
         Thread.sleep(1500L)
         shoot()
         Thread.sleep(1500L)
         chargeBullet(Bullet.RED)
+        Thread.sleep(1500L)
+        shoot()
         Thread.sleep(1500L)
         shoot()
         Thread.sleep(1500L)
