bb6ebb475fd3feb4a6376beef3cad65ae9f9a684
tutyperefruty <tutyperefruty@gmail.com>
2025-12-20 08:30:34 +0900

    KS-19-5 refactor

--- src/main/kotlin/lesson_19/lesson19_task5.kt
+++ src/main/kotlin/lesson_19/lesson19_task5.kt
@@ -5,16 +5,35 @@ const val LISTING_SIZE = 5
 enum class Sex { MALE, FEMALE }
 
 class PersonFile(
-    private val name: String,
-    private val sex: Sex?
+    private val _name: String,
+    private val _sex: Sex
 ) {
-    companion object {
-        fun displayFilesList(personFilesList: MutableList<PersonFile>) {
-            if (personFilesList.size == LISTING_SIZE) {
-                personFilesList.forEachIndexed { index, file ->
-                    println("${index + 1}: ${file.name} - ${file.sex}")
-                }
-            }
+    val name: String
+        get() = _name
+    val sex: Sex
+        get() = _sex
+}
+
+fun readSex(): Sex {
+    while (true) {
+        println(
+            "Enter the gender [Instructions: Enter only in English in all cases, e.g., mAle, FemalE. " +
+                    "You will reenter gender until it correct]: "
+        )
+        val inputSex: String = readln().lowercase()
+        val sex = when (inputSex) {
+            Sex.MALE.name.lowercase() -> Sex.MALE
+            Sex.FEMALE.name.lowercase() -> Sex.FEMALE
+            else -> null
+        }
+        if (sex != null) return sex
+    }
+}
+
+fun displayFilesList(personFilesList: MutableList<PersonFile>) {
+    if (personFilesList.size == LISTING_SIZE) {
+        personFilesList.forEachIndexed { index, file ->
+            println("${index + 1}: ${file.name} - ${file.sex}")
         }
     }
 }
@@ -26,19 +45,11 @@ fun main() {
         println("Enter Name how you want. Blank does mean 'No-named':")
         val inputName: String = readln().ifBlank { "NO_NAME" }
 
-        println(
-            "Enter the gender [Instructions: Enter only in English in all cases, e.g., mAle, FemalE. " +
-                    "If you enter anything else or nothing, your sex will be set as 'null']: "
-        )
-        val inputSex: String = readln().lowercase()
-        val sex = when (inputSex) {
-            Sex.MALE.name.lowercase() -> Sex.MALE
-            Sex.FEMALE.name.lowercase() -> Sex.FEMALE
-            else -> null
-        }
+        val finalSex = readSex()
 
-        personFilesList.add(PersonFile(name = inputName, sex = sex))
-
-        PersonFile.displayFilesList(personFilesList)
+        personFilesList.add(PersonFile(_name = inputName, _sex = finalSex))
     }
+
+    displayFilesList(personFilesList)
 }
+
