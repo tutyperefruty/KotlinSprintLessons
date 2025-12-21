e5a638c5c320ced4774455a88b82e0fbfdd7b7c0
tutyperefruty <tutyperefruty@gmail.com>
2025-12-21 17:39:31 +0900

    KS-20-3 refactor

--- src/main/kotlin/lesson_20/lesson20_task3.kt
+++ src/main/kotlin/lesson_20/lesson20_task3.kt
@@ -1,35 +1,33 @@
 package org.example.lesson_20
 
-class Player(
-    private val _name: String = "Player-#",
-    private var _keyPresence: Boolean = false,
-) {
-    val name: String
-        get() = _name
-    var keyPresence: Boolean
-        get() = _keyPresence
+class Player {
+    val name: String = "Player-#"
+        get() = field
+    var keyPresence: Boolean = false
+        get() = field
         set(value) {
-            _keyPresence = value
+            field = value
         }
 }
 
-val checkKey: (Player) -> Unit = { gamer: Player ->
-    if (gamer.keyPresence) {
-        println("${gamer.name} opened the door")
-    } else {
-        println("The door is locked")
-    }
-}
 
-fun getKey(player: Player) {
+fun isKey(player: Player) {
     player.keyPresence = true
 }
 
 fun main() {
+    val checkKey: (Player) -> Unit = { gamer: Player ->
+        if (gamer.keyPresence) {
+            println("${gamer.name} opened the door")
+        } else {
+            println("The door is locked")
+        }
+    }
+
     val player1 = Player()
     with(player1) {
         checkKey(this)
-        getKey(this)
+        isKey(this)
         checkKey(this)
     }
 }
