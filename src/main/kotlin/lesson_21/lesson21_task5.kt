1991e6de9682365e493a68684d8b141120354315
tutyperefruty <tutyperefruty@gmail.com>
2025-12-26 07:44:32 +0900

    KS-21-5 Map<K,V>.maxCategory()

---
+++ src/main/kotlin/lesson_21/lesson21_task5.kt
@@ -0,0 +1,20 @@
+package org.example.lesson_21
+
+fun Map<String, Int>.maxCategory(): String {
+    return this.maxByOrNull { it.value }?.run { key ?: "Empty key" } ?: "Empty map"
+}
+
+fun main() {
+
+    val experience: Map<String, Int> = mapOf(
+        "Breath" to 900,
+        "Smile" to 1000,
+        "Jump" to 1200,
+        "Roll" to 800,
+        "Speak" to 300,
+        "Write" to 1200,
+        "Fly" to 0
+    )
+    println(experience.maxCategory())
+
+}
