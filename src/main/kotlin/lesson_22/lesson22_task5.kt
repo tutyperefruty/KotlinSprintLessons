51d8d19b61826e40876d32c3dd1ca8afe2132a47
tutyperefruty <tutyperefruty@gmail.com>
2025-12-30 13:04:54 +0900

    KS-22-5 refactor

--- src/main/kotlin/lesson_22/lesson22_task5.kt
+++ src/main/kotlin/lesson_22/lesson22_task5.kt
@@ -10,16 +10,30 @@ data class GalacticGuide(
 )
 
 fun main() {
+    val guideList: MutableList<GalacticGuide> = mutableListOf()
     val alphaCentauri = GalacticGuide(
         location = "The constellations of Centaurus",
         description = "A new object will be detected closer to the Rigel",
         eventDateTime = LocalDateTime.of(2032, 10, 7, 19, 7),
         distanceFromEarth = 4.36
     )
-    val (location, description, eventDateTime, distanceFromEarth) = alphaCentauri
+    guideList.add(alphaCentauri)
 
-    println("Locsation: $location")
-    println("Description: $description")
-    println("Date and time: $eventDateTime")
-    println("Distance from Earth: $distanceFromEarth ly")
+    //    Только для Альфа Центавра
+    val place = alphaCentauri.component1()
+    val withDescription = alphaCentauri.component2()
+    val inDate = alphaCentauri.component3()
+    val showPlaceInfo: () -> Unit = {
+        println("Here's $place where $withDescription in ${inDate.year}")
+    }
+    showPlaceInfo()
+
+    //    Для всех в списке
+    val showPlaceDistanceInfoIn: (List<GalacticGuide>) -> Unit = {
+        for ((_, _, _, distance) in guideList) {
+            print("$place: Distance from Earth is $distance. ")
+            if (distance > 1) println("It's rather far from Earth") else println("It's close to Earth")
+        }
+    }
+    showPlaceDistanceInfoIn(guideList)
 }
