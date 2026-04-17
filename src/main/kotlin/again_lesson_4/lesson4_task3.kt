da3d523558b8ab82350686753c1d2b477cf65676
tutyperefruty <tutyperefruty@gmail.com>
2026-04-18 00:54:05 +0300

    KS-4-3 display result for growing (refactor)

--- src/main/kotlin/again_lesson_4/lesson4_task3.kt
+++ src/main/kotlin/again_lesson_4/lesson4_task3.kt
@@ -1,20 +1,20 @@
 package org.example.again_lesson_4
 
-const val WEATHER: String = "sun"
-const val IS_FOR_OPENED_TENT: Boolean = true
-const val HUMIDITY: Int = 20
-const val IS_FOR_WINTER_SEASON: Boolean = false
+const val SUNNY_WEATHER: Boolean = true
+const val OPENED_TENT: Boolean = true
+const val FAVORABLE_HUMIDITY: Int = 20
+const val WINTER: String = "Winter"
 const val RESULT_QUESTION = "Are the conditions now favorable for growing legumes?"
 
 fun main() {
 
-    val todayWeather: String = "sun"
+    val isSunnyToday: Boolean = true
     val isNowTentOpened: Boolean = true
     val presentHumidity: Int = 20
-    val isNowWinter: Boolean = true
+    val currentSeason: String = "Winter"
 
-    val result = (todayWeather == WEATHER) && (isNowTentOpened == IS_FOR_OPENED_TENT)
-            && (presentHumidity == HUMIDITY) && (isNowWinter == IS_FOR_WINTER_SEASON)
+    val result = isSunnyToday == SUNNY_WEATHER && isNowTentOpened == OPENED_TENT
+            && presentHumidity == FAVORABLE_HUMIDITY && currentSeason != WINTER
 
     println("$RESULT_QUESTION $result")
 }
