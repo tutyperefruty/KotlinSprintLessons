dd56f2b8a4352b0e90a1bd0fa39dbd2427869062
tutyperefruty <tutyperefruty@gmail.com>
2026-04-19 08:35:52 +0300

    KS-4-4 display exercise program with inversion (refactor)

--- src/main/kotlin/again_lesson_4/lesson4_task4.kt
+++ src/main/kotlin/again_lesson_4/lesson4_task4.kt
@@ -1,20 +1,20 @@
 package org.example.again_lesson_4
 
-const val CONSTANT_FOR_ARMS_AND_ABS = 1
+const val ZERO = 0
 const val REMAINDER_OF_TWO = 2
 
 fun main() {
 
     val todayIsDay: Int = 5
 
-    val exerciseReminder: Boolean = todayIsDay % REMAINDER_OF_TWO == CONSTANT_FOR_ARMS_AND_ABS
+    val isEvenDay: Boolean = todayIsDay % REMAINDER_OF_TWO == ZERO
 
     println(
         """
-        Exercises for hands: $exerciseReminder
-        Exercises for abs:   $exerciseReminder
-        Exercises for legs:  ${!exerciseReminder}
-        Exercises for back:  ${!exerciseReminder}
+        Exercises for hands: ${!isEvenDay}
+        Exercises for legs:  $isEvenDay
+        Exercises for back:  $isEvenDay
+        Exercises for abs:   ${!isEvenDay}
     """.trimIndent()
     )
 }
