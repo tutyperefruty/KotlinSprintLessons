8388d90a55246508ee58895fda82fb1d5b2d5534
tutyperefruty <tutyperefruty@gmail.com>
2026-04-20 18:55:04 +0300

    KS-4-5 ship departure (refactor)

--- src/main/kotlin/again_lesson_4/lesson4_task5.kt
+++ src/main/kotlin/again_lesson_4/lesson4_task5.kt
@@ -4,20 +4,16 @@ const val CREW_MIN = 55
 const val CREW_MAX = 70
 const val STD_CREW = CREW_MAX
 const val STD_PROVISION_BOX = 50
-const val TRUE = "true"
-const val FALSE = "false"
 const val POSSIBLE_FOR_DEPARTURE = "The ship can depart"
 const val IMPOSSIBLE_FOR_DEPARTURE = "The ship can't depart"
 
 fun main() {
 
     println("Input: the ship has damage (yes or no): ")
-    val hasDamageString: String = if (readln().lowercase() == "yes") TRUE else FALSE
-    val hasDamage: Boolean = hasDamageString.toBoolean()
+    val hasDamage: Boolean = readln().toBoolean()
 
     println("Input: are there favorable weather conditions (yes or no): ")
-    val isWeatherCondition = if (readln().lowercase() == "yes") TRUE else FALSE
-    val isFavorableWeatherCondition: Boolean = isWeatherCondition.toBoolean()
+    val isFavorableWeatherCondition: Boolean = readln().toBoolean()
 
     println("Input: number of crew: ")
     val currentCrewNumber: Int = readln().toInt()
@@ -25,7 +21,9 @@ fun main() {
     println("Input: number of provision box: ")
     val currentProvisionBoxNumber: Int = readln().toInt()
 
-    val canDepartForLong: Boolean = !hasDamage &&
+    val isWeatherOkForLong: Boolean = isFavorableWeatherCondition || !isFavorableWeatherCondition
+
+    val canDepartForLong: Boolean = !hasDamage && isWeatherOkForLong &&
             currentCrewNumber in CREW_MIN..CREW_MAX && currentProvisionBoxNumber > STD_PROVISION_BOX
     val canDepartAlternative: Boolean = isFavorableWeatherCondition &&
             currentCrewNumber == STD_CREW && currentProvisionBoxNumber >= STD_PROVISION_BOX
