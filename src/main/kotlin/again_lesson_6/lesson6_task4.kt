a8e709314c337e050045000bf43352cf4f0301f4
tutyperefruty <tutyperefruty@gmail.com>
2026-04-23 09:18:53 +0300

    KS-6-4 display matching-game result

---
+++ src/main/kotlin/again_lesson_6/lesson6_task4.kt
@@ -0,0 +1,28 @@
+package org.example.again_lesson_6
+
+const val MIN_NUM = 1
+const val MAX_NUM = 9
+
+fun main() {
+
+    var attempt: Int = 5
+    val randomNumber = (MIN_NUM..MAX_NUM).random()
+
+    do {
+        println("\nВведи любое число от $MIN_NUM до $MAX_NUM: ")
+        val inputNumber = readln().toInt()
+
+        if (inputNumber == randomNumber) {
+            println("Это была великолепная игра!")
+            break
+        } else {
+            attempt--
+            if (attempt == 0) {
+                println("It was number $randomNumber")
+                break
+            }
+            println("Неверно, оставшееся количество попыток: $attempt")
+        }
+    } while (true)
+
+}
