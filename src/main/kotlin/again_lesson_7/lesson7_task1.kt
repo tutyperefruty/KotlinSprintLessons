b48149b1b91f2e69046acc18760a1e57a368f506
tutyperefruty <tutyperefruty@gmail.com>
2026-04-23 10:37:08 +0300

    KS-7-1 display generated 6-chars password

---
+++ src/main/kotlin/again_lesson_7/lesson7_task1.kt
@@ -0,0 +1,25 @@
+package org.example.again_lesson_7
+
+const val MIN_NUM = 0
+const val MAX_NUM = 9
+const val MIN_SMALL_CHAR = 'a'
+const val MAX_SMALL_CHAR = 'z'
+const val MIN_BIG_CHAR = 'A'
+const val MAX_BIG_CHAR = 'Z'
+const val PASS_FROM = 1
+const val PASS_TO = 6
+
+fun main() {
+
+    var password = ""
+    for (output in PASS_FROM..PASS_TO) {
+        val randomOfRandoms = listOf(
+            (MIN_NUM..MAX_NUM).random(),
+            (MIN_SMALL_CHAR..MAX_SMALL_CHAR).random(),
+            (MIN_BIG_CHAR..MAX_BIG_CHAR).random()
+        )
+            .random()
+        password += randomOfRandoms
+    }
+    println(password)
+}
