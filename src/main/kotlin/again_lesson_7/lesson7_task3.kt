9b656dff9ded07f8482c732b29ec4b9e316b2fa1
tutyperefruty <tutyperefruty@gmail.com>
2026-04-24 22:44:43 +0300

    KS-7-3 display even numbers (refactor)

--- src/main/kotlin/again_lesson_7/lesson7_task3.kt
+++ src/main/kotlin/again_lesson_7/lesson7_task3.kt
@@ -1,11 +1,9 @@
 package org.example.again_lesson_7
 
-const val RANGE_START_NUM = 0
-
 fun main() {
 
     println("Введи число: ")
     val rangeLastNumber = readln().toInt()
 
-    for (number in RANGE_START_NUM until rangeLastNumber step 2) println(number)
+    for (number in 0 .. rangeLastNumber step 2) println(number)
 }
