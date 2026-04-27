4973ef44d8332310ab056be4ce97c0a59013b0bb
tutyperefruty <tutyperefruty@gmail.com>
2026-04-27 09:48:49 +0300

    KS-7-1 generate and display 6-length password (refactor 4)

--- src/main/kotlin/again_lesson_7/lesson7_task1.kt
+++ src/main/kotlin/again_lesson_7/lesson7_task1.kt
@@ -1,20 +1,16 @@
 package org.example.again_lesson_7
 
-const val MIN_NUM = 0
-const val MAX_NUM = 9
-const val MIN_SMALL_CHAR = 'a'
-const val MAX_SMALL_CHAR = 'z'
 const val PASS_FROM = 1
 const val PASS_TO = 6
 
 fun main() {
 
     var password = ""
-    val charRange = MIN_SMALL_CHAR..MAX_SMALL_CHAR
-    val digitRange = MIN_NUM..MAX_NUM
+    val charRange = 'a'..'z'
+    val digitRange = 0..9
 
     for (i in PASS_FROM..PASS_TO)
-        password += if (i % 2 == 0) charRange.random().toString() else digitRange.random().toString()
+        password += if (i % 2 == 0) charRange.random() else digitRange.random().toString()
 
     println(password)
 }
