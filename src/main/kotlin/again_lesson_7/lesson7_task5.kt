ebab48e53adeeb6ed8756e999a7bea8ba1b8b898
tutyperefruty <tutyperefruty@gmail.com>
2026-04-27 10:00:40 +0300

    KS-7-5 generate and display complex password with minimum 6 symbols (refactor 2)

--- src/main/kotlin/again_lesson_7/lesson7_task5.kt
+++ src/main/kotlin/again_lesson_7/lesson7_task5.kt
@@ -1,12 +1,6 @@
 package org.example.again_lesson_7
 
 const val MIN_PASSWORD_LENGTH = 6
-const val MIN_NUM = '0'
-const val MAX_NUM = '9'
-const val MIN_SMALL_CHAR = 'a'
-const val MAX_SMALL_CHAR = 'z'
-const val MIN_BIG_CHAR = 'A'
-const val MAX_BIG_CHAR = 'Z'
 
 fun main() {
 
@@ -14,16 +8,16 @@ fun main() {
     var passwordLength = readln().toInt()
     if (passwordLength < MIN_PASSWORD_LENGTH) passwordLength = MIN_PASSWORD_LENGTH
 
-    val digitToCharRange = MIN_NUM..MAX_NUM
-    val smallCharRange = MIN_SMALL_CHAR..MAX_SMALL_CHAR
-    val bigCharRange = MIN_BIG_CHAR..MAX_BIG_CHAR
-    val fullRange = digitToCharRange.toList() + smallCharRange.toList() + bigCharRange.toList()
+    val digitToCharRange = '0'..'9'
+    val smallCharRange = 'a'..'z'
+    val bigCharRange = 'A'..'Z'
+    val fullRange = digitToCharRange + smallCharRange + bigCharRange
 
     val symbolListForPassword = mutableListOf<Char>()
 
-    (0 until passwordLength).forEachIndexed { index, _ ->
+    for (i in 0 until passwordLength) {
         symbolListForPassword +=
-            when (index % 4) {
+            when (i % 4) {
                 0 -> digitToCharRange.random()
                 1 -> smallCharRange.random()
                 2 -> bigCharRange.random()
