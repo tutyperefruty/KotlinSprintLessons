7e846ebc20d86b73e081c64b8a2f88254f06b23c
tutyperefruty <tutyperefruty@gmail.com>
2026-04-28 08:56:51 +0300

    KS-9-2 add element to list and display all

---
+++ src/main/kotlin/again_lesson_9/lesson9_task2.kt
@@ -0,0 +1,19 @@
+package org.example.again_lesson_9
+
+fun main() {
+    val ingredients = mutableListOf("Томат", "Огурец", "Лук")
+    println("В рецепте есть базовые ингредиенты: $ingredients")
+    println("Желаете добавить еще? ")
+    val newIngredient = readln()
+
+    if (newIngredient.equals("да", ignoreCase = true)) {
+        println("Какой ингредиент вы хотите добавить? ")
+        val newIngredient = readln()
+        if (ingredients.any { ingredient -> ingredient.equals(newIngredient, ignoreCase = true) }) {
+            println("Ингредиент '$newIngredient' содержится в списке")
+        } else {
+            ingredients.add(newIngredient)
+            println("Теперь в рецепте есть следующие ингредиенты: $ingredients")
+        }
+    }
+}
