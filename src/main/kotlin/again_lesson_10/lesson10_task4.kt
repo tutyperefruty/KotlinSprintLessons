47c57d0b2a4b38619f09239c6349b76d955acf04
tutyperefruty <tutyperefruty@gmail.com>
2026-05-01 11:25:49 +0300

    KS-10-4 display human-computer's game result (global refactor)

--- src/main/kotlin/again_lesson_10/lesson10_task4.kt
+++ src/main/kotlin/again_lesson_10/lesson10_task4.kt
@@ -7,35 +7,51 @@ fun main() {
     var humanWins = 0
 
     while (true) {
-        val roundResult = playRound(playerRollDice = rollDice(), computerRollDice = rollDice())
-
-        when (roundResult) {
-            true -> println("Побеждает человечество").also { humanWins++ }
-            false -> println("Машина победила")
-            else -> println("Победила дружба")
-        }
-
-        println("Хотите сыграть еще раунд? Введите Да или Нет")
-        val answer = readln()
-        if (answer.equals("да", ignoreCase = true)) continue
-        else if (answer.equals("нет", ignoreCase = true))
-            println("Финиш\nОбщее количество побед человечества: $humanWins").also { return }
-        else return
+        if (playRound(playerRollDice = rollDice(), computerRollDice = rollDice())) humanWins++
+        if (askForPlayAgain(humanWins)) continue else return
     }
 }
 
-private fun playRound(playerRollDice: Int, computerRollDice: Int): Boolean? {
+private fun askForPlayAgain(humanWins: Int): Boolean {
+    println("Хотите сыграть еще раунд? Введите Да или Нет")
+    val answer = readln()
+    if (answer.equals("да", ignoreCase = true)) return true
+    else if (answer.equals("нет", ignoreCase = true)) displayStatsBeforeEnd(humanWins).also { return false }
+    else displayStatsBeforeEnd(humanWins)
+    return false
+}
 
+private fun displayStatsBeforeEnd(humanWins: Int) = println("Финиш\nОбщее количество побед человечества: $humanWins")
+
+private fun playRound(playerRollDice: Int, computerRollDice: Int): Boolean {
+
+    displayRoundProgress(playerRollDice, computerRollDice)
+
+    return displayRoundResult(determineWinner(playerRollDice, computerRollDice))
+}
+
+private fun displayRoundProgress(playerRollDice: Int, computerRollDice: Int) {
     println(
         "Человек кидает кости ->\nУ человека выпало: $playerRollDice\n" +
                 "Машина кидает кости ->\nУ машины выпало: $computerRollDice"
     )
+}
 
+private fun determineWinner(playerRollDice: Int, computerRollDice: Int): Boolean? {
     return when {
         playerRollDice > computerRollDice -> true
         playerRollDice < computerRollDice -> false
         else -> null
     }
+}
+
+private fun displayRoundResult(result: Boolean?): Boolean {
+    when (result) {
+        true -> println("Побеждает человечество").also { return true }
+        false -> println("Машина победила")
+        else -> println("Победила дружба")
+    }
+    return false
 }
 
 private fun rollDice(): Int = (DICE_MIN_VALUE..DICE_MAX_VALUE).random()
