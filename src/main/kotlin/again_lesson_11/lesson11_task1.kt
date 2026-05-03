d31c4abdd9d58a784a71b857c01672650b94fd2c
tutyperefruty <tutyperefruty@gmail.com>
2026-05-03 09:36:15 +0300

    KS-11-1 create class (refactor)

--- src/main/kotlin/again_lesson_11/lesson11_task1.kt
+++ src/main/kotlin/again_lesson_11/lesson11_task1.kt
@@ -1,17 +1,15 @@
 package org.example.again_lesson_11
 
-
 class User(
     val id: Int,
     val login: String,
     val password: String,
-    val eMail: String,
+    val email: String,
 ) {
-    fun displayUserInfo() = println("Юзер с id $id: login - $login, password - $password, e-mail - $eMail.")
+    fun displayUserInfo() = println("Юзер с id $id: login - $login, password - $password, e-mail - $email.")
 }
 
 fun main() {
-
     val userLola = User(1, "Лола", "fghjkl", "lola@kotlin.kot")
     val userZaphod = User(2, "Зафод", "xcvbnm", "zaphod@kotlin.kot")
 
