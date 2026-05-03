3af675efcc00414f826dbce0d55fbd96045731ca
tutyperefruty <tutyperefruty@gmail.com>
2026-05-03 09:31:45 +0300

    KS-10-5 display result of authorization getting cart (refactor)

--- src/main/kotlin/again_lesson_10/lesson10_task5.kt
+++ src/main/kotlin/again_lesson_10/lesson10_task5.kt
@@ -2,30 +2,21 @@ package org.example.again_lesson_10
 
 const val LOGIN = "l"
 const val PASSWORD = "p"
-const val LOGIN_NAME = "Login"
-const val PASS_NAME = "Password"
-const val TOKEN_NAME = "Token"
 
 fun main() {
 
-    val cart = listOf("Кукла", 7, "Банка", 9, "Ракета")
+    println("Введите Login")
+    val inputLogin = readln().trim()
+    println("Введите password")
+    val inputPassword = readln().trim()
 
-    askFor(LOGIN_NAME)
-    if (readln() validateWith LOGIN == null) printValidFail(LOGIN_NAME).also { return }
-
-    askFor(PASS_NAME)
-    if (readln() validateWith PASSWORD == null) printValidFail(PASS_NAME).also { return }
-
-    askFor(TOKEN_NAME)
-    val tokenString = generateToken().also { println(it) }
-    if (readln() validateWith tokenString == null) printValidFail(TOKEN_NAME).also { return }
-    else getCart(cart)
+    val token = validateLoginAndPassword(inputLogin, inputPassword)
+    if (token != null) println("Сейчас в корзине присутствуют: ${getCart(token)}")
+    else println("Validation failed")
 }
 
-private fun askFor(string: String) = println("Введите $string")
-
-private infix fun String?.validateWith(infoString: String): String? =
-    if (this?.isBlank() == true || this != infoString) null else ""
+private fun validateLoginAndPassword(login: String, password: String)=
+    if (login == LOGIN && password == PASSWORD) generateToken() else null
 
 private fun generateToken(): String {
     val stringBuilder = StringBuilder()
@@ -34,6 +25,5 @@ private fun generateToken(): String {
     return stringBuilder.toString()
 }
 
-private fun printValidFail(smth: String) = println("$smth validation failed")
-
-private fun getCart(cart: List<Any>) = println("Сейчас в корзине присутствуют: $cart")
+private fun getCart(token: String): List<Any>  =
+    if (token.isNotBlank()) listOf("Кукла", 7, "Банка", 9, "Ракета") else emptyList()
